00c6785a0d593725cafddc6c232d22118fe6f3fc
{txegg} <{fksk94@gmail.com}>
2020-07-19 11:57:43 +0900

    commit test

--- weather.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-key = "정해야 되욤~"
-city = "Gumi"
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}'
-
-response = requests.get(url).json()
-
-print(response)
